79714919750da1fde18e71e4ba96072ff56d60b4
V3nts0 <ventsi.d.georgiev@gmail.com>
2021-01-28 11:05:27 +0200

    Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(source) {
+    document.querySelector('.starbucks').src = source;
+}
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color
+}
